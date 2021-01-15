bad2be81e03323b541bdfcbb747e1b2581c77ec0
tgd15 <tgd15@case.edu>
2021-01-15 17:05:22 -0500

    Removed old code; updated with new functions

--- run_Unet.py
+++ run_Unet.py
@@ -6,18 +6,14 @@ Created on Fri Jan 15 15:34:51 2021
 @author: Tom
 """
 
-import load_model as lm
+import load_data as ld
+import load_Unet as lm
 
 Model_Name = '/Volumes/GoogleDrive/Shared drives/INVent/tom/Post-CRT_Unet/Training_Lumen_Unet.hdf5'
 
-print ("Predicting on original masks...")
-holdoutTest = '/Volumes/GoogleDrive/My Drive/tom/Rectal Segmentation/Data-Revised/Testing/Datasets/Lumen_Testing_Dataset_original_masks.hdf5'
-predictionName = 'Lumen_Preds_Original_Masks.hdf5'
-lm.load_and_predict(Model_Name,holdoutTest,predictionName)
-print ("Saved predictions \n")
+image_path = '/Volumes/GoogleDrive/Shared drives/INVent_Data/Rectal/newdata/UH/RectalCA130-2/RectalCA130-2_Post_Ax.mha'
+image = ld.load_image(image_path)
+unet = lm.load_Unet_model(Model_Name)
 
-print ("Predicting on updated masks...")
-holdoutTest = '/Volumes/GoogleDrive/My Drive/tom/Rectal Segmentation/Data-Revised/Testing/Datasets/Lumen_Testing_Dataset_updated_masks.hdf5'
-predictionName = 'Lumen_Preds_Updated_Masks.hdf5'
-lm.load_and_predict(Model_Name,holdoutTest,predictionName)
-print ("Saved predictions")
+print ("Predicting on image...")
+predictions = unet.predict(image, verbose=1)
