9dd7659a6dbf4ca407ecee34a363585498891581
Tom DeSilvio <tgd15@case.edu>
2022-12-08 19:45:59 -0500

    Added scripts for visualizing multiclass results

---
+++ 2D_Figures_Dice_per_Slice_multiclass_readers.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 23 13:02:12 2022
+
+@author: Tom
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+def parse_excel(excel_path):
+    
+    xls = pd.ExcelFile(excel_path)
+    xls_dict = {}
+    for sheet_name in xls.sheet_names:
+        xls_dict[sheet_name] = xls.parse(sheet_name)
+        
+    return xls_dict
+
+# Read in ORW DSC scores
+orw_expert1_path = "../Multiclass U-Net/Results/2022-03-31/orw/CCA_Expert1/Dice_Per_Slice.xlsx"
+orw_expert2_path = "../Multiclass U-Net/Results/2022-03-31/orw/CCA_Expert2/Dice_Per_Slice.xlsx"
+orw_reader_path = "../Outer Rectal Wall U-Net/Results/ExperttoExpert/Dice_Per_Slice.xlsx"
+
+orw_expert1_dsc = parse_excel(orw_expert1_path)
+orw_expert2_dsc = parse_excel(orw_expert2_path)
+orw_reader_dsc = parse_excel(orw_reader_path)
+
+# Read in Lumen DSC scores
+lumen_expert1_path = "../Multiclass U-Net/Results/2022-03-31/lumen/CCA_Expert1/Dice_Per_Slice.xlsx"
+lumen_expert2_path = "../Multiclass U-Net/Results/2022-03-31/lumen/CCA_Expert2/Dice_Per_Slice.xlsx"
+lumen_reader_path = "../Lumen U-Net/Results/ExperttoExpert/Dice_Per_Slice.xlsx"
+
+lumen_expert1_dsc = parse_excel(lumen_expert1_path)
+lumen_expert2_dsc = parse_excel(lumen_expert2_path)
+lumen_reader_dsc = parse_excel(lumen_reader_path)
+
+# Read in Fat DSC scores
+fat_expert1_path = "../Multiclass U-Net/Results/2022-12-01/fat/CCA_Expert1/Dice_Per_Slice.xlsx"
+fat_expert2_path = "../Multiclass U-Net/Results/2022-12-01/fat/CCA_Expert2/Dice_Per_Slice.xlsx"
+fat_reader_path = "../Fat U-Net/Results/2022-11-23/ExperttoExpert/Dice_Per_Slice.xlsx"
+
+fat_expert1_dsc = parse_excel(fat_expert1_path)
+fat_expert2_dsc = parse_excel(fat_expert2_path)
+fat_reader_dsc = parse_excel(fat_reader_path)
+
+# Get list of patients
+pt_dir = "/Volumes/GoogleDrive/My Drive/tom/Rectal Segmentation/Data-MultipleExperts/Testing/Volumes/"
+pt_files = sorted([os.path.splitext(filename)[0] for filename in os.listdir(pt_dir)])
+
+# Loop through each patient and plot DSC scores
+i = 0
+for patient in pt_files:
+    
+    # Get DSC scores
+    orw_expert1 = orw_expert1_dsc[patient]
+    lumen_expert1 = lumen_expert1_dsc[patient]
+    fat_expert1 = fat_expert1_dsc[patient]
+
+    orw_expert2 = orw_expert2_dsc[patient]
+    lumen_expert2 = lumen_expert2_dsc[patient]
+    fat_expert2 = fat_expert2_dsc[patient]
+ 
+    # Initialize plot
+    fig, ax = plt.subplots(sharey = True, figsize = (10, 10))
+    
+    # PLOT 1
+    # Plot lumen DSC
+    x = list(lumen_expert1.diceInfo_1)
+    x = [ele.split("_")[-1] for ele in x]
+    y = lumen_expert1.diceInfo_2
+    
+    ax.plot(x, y, label = "Lumen Expert 1", marker = "o", color = "saddlebrown")
+    
+    # Plot ORW DSC
+    x = list(orw_expert1.diceInfo_1)
+    y = orw_expert1.diceInfo_2
+    x = [ele.split("_")[-1] for ele in x]
+    ax.plot(x, y, label = "ORW Expert 1", marker = "o", color = "blue")
+    
+    # Plot fat DSC
+    # Some slice of lumen/ORW have no fat.
+    # This will cause a mismatch of x andy dimensions on plot
+    # Alter fat DSC dataframe to have same number of rows as ORW dataframe
+    if(len(x) != fat_expert1.diceInfo_1.shape[0]):
+        difference = orw_expert1.loc[~orw_expert1['diceInfo_1'].isin(fat_expert1['diceInfo_1'])]
+        difference.loc[difference['diceInfo_2'] > 0.0, "diceInfo_2"] = 0
+        
+        fat_expert1 = fat_expert1.append(difference, ignore_index = True)
+        fat_expert1 = fat_expert1.sort_values(by = ["diceInfo_1"])
+
+    y = fat_expert1.diceInfo_2
+    
+    ax.plot(x, y, label = "Fat Expert 1", marker = "o", color = "green")
+    ax.set_xticklabels(x)
+    ax.yaxis.set_ticks(np.arange(0, 1, 0.1))
+    ax.set_xlabel("Slice Number")
+    ax.set_ylabel("DSC")
+    ax.set_title(patient)
+    
+    # PLOT 2
+    # Plot lumen DSC
+    x = list(lumen_expert2.diceInfo_1)
+    x = [ele.split("_")[-1] for ele in x]
+    y = lumen_expert2.diceInfo_2
+    
+    ax.plot(x, y, label = "Lumen Expert 2", marker = "s", color = "saddlebrown", linestyle = ":")
+    
+    # Plot ORW DSC
+    x = list(orw_expert2.diceInfo_1)
+    x = [ele.split("_")[-1] for ele in x]
+    y = orw_expert2.diceInfo_2
+    ax.plot(x, y, label = "ORW Expert 2", marker = "s", color = "blue", linestyle = ":")
+    
+    # Plot fat DSC
+    # Some slice of lumen/ORW have no fat.
+    # This will cause a mismatch of x andy dimensions on plot
+    # Alter fat DSC dataframe to have same number of rows as ORW dataframe
+    if(len(x) != fat_expert2.diceInfo_1.shape[0]):
+        difference = orw_expert2.loc[~orw_expert2['diceInfo_1'].isin(fat_expert2['diceInfo_1'])]
+        difference.loc[difference['diceInfo_2'] > 0.0, "diceInfo_2"] = 0
+        
+        fat_expert2 = fat_expert2.append(difference, ignore_index = True)
+        fat_expert2 = fat_expert2.sort_values(by = ["diceInfo_1"])
+
+    y = fat_expert2.diceInfo_2
+    
+    ax.plot(x, y, label = "Fat Expert 2", marker = "s", color = "green", linestyle = ":")
+
+    ax.grid()
+    ax.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
+    ax.set_xticklabels(x)
+    ax.set_xlabel("Slice Number")
+    ax.set_ylabel("DSC")
+    
+    fig.savefig("2D Figures/Comparisons/Multiclass/Readers/" + patient + ".png", dpi = 300, bbox_inches = "tight")
+
+    
---
+++ 2D_Figures_Multiclass_readers_from_npy.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 17 15:35:16 2021
+
+@author: Tom
+"""
+
+import numpy as np
+import scipy.io as sio
+import os
+import h5py
+import matplotlib.pyplot as plt
+
+# Specify filepaths
+unet = input("Unet Name: ")
+dataset = input("Dataset Name: ")
+
+if(unet == "Fat"):
+    seg1_path = "/Volumes/GoogleDrive/My Drive/tom/Rectal Segmentation/Data-MultipleExperts/" + unet + " U-Net/U_Net Code/" + unet + "_Unet_Predictions_Expert_1.npy"
+    seg2_path = "/Volumes/GoogleDrive/My Drive/tom/Rectal Segmentation/Data-MultipleExperts/" + unet + " U-Net/U_Net Code/" + unet + "_Unet_Predictions_Expert_2.npy"
+    expert1_path = "/Volumes/GoogleDrive/My Drive/tom/Rectal Segmentation/Data-MultipleExperts/Fat U-Net/U_Net Code/" + unet + "_Unet_Gt_Expert_1.npy"
+    expert2_path = "/Volumes/GoogleDrive/My Drive/tom/Rectal Segmentation/Data-MultipleExperts/Fat U-Net/U_Net Code/" + unet + "_Unet_Gt_Expert_2.npy"
+    root_out = "/Volumes/GoogleDrive/My Drive/tom/Rectal Segmentation/Data-MultipleExperts/Figures/2D Figures/Multiple_Experts/" + unet + "/"
+else:
+    seg1_path = "/Volumes/GoogleDrive/My Drive/tom/Rectal Segmentation/Data-MultipleExperts/" + unet + " U-Net/U_Net Code/" + dataset + "_Unet_Predictions_Expert_1.npy"
+    seg2_path = "/Volumes/GoogleDrive/My Drive/tom/Rectal Segmentation/Data-MultipleExperts/" + unet + " U-Net/U_Net Code/" + dataset + "_Unet_Predictions_Expert_2.npy"
+    expert1_path = "/Volumes/GoogleDrive/My Drive/tom/Rectal Segmentation/Data-MultipleExperts/" + unet + " U-Net/U_Net Code/" + dataset + "_Unet_Gt_Expert_1.npy"
+    expert2_path = "/Volumes/GoogleDrive/My Drive/tom/Rectal Segmentation/Data-MultipleExperts/" + unet + " U-Net/U_Net Code/" + dataset + "_Unet_Gt_Expert_2.npy"
+    root_out = "/Volumes/GoogleDrive/My Drive/tom/Rectal Segmentation/Data-MultipleExperts/Figures/2D Figures/Multiple_Experts/" + dataset+ "/"
+    
+images1_path = "/Volumes/GoogleDrive/My Drive/tom/Rectal Segmentation/Data-MultipleExperts/Testing/Datasets/" + dataset + "_Testing_Dataset_expert1.hdf5"
+images2_path = "/Volumes/GoogleDrive/My Drive/tom/Rectal Segmentation/Data-MultipleExperts/Testing/Datasets/" + dataset + "_Testing_Dataset_expert2.hdf5"
+pt_names_path = "/Volumes/GoogleDrive/My Drive/tom/Rectal Segmentation/Data-MultipleExperts/Testing/Volumes/"
+
+# Get list of patient names
+
+pt_names = os.listdir(pt_names_path)
+pt_names = sorted([name.replace(".mha","") for name in pt_names])
+try:
+    pt_names.remove(".DS_Store")
+except Exception as e:
+    print(".DS_Store not present in patinet list. yay!")
+
+# Read in cropped images
+
+with h5py.File(images1_path, 'r') as f:
+        images1 = f['testing_Images'][()]
+        filenames1 = f['testing_image_filenames'][()]
+        
+images1 = images1.squeeze()
+filenames1 = filenames1.tolist()
+filenames1=[x.decode('utf-8') for x in filenames1]
+        
+with h5py.File(images2_path, 'r') as f:
+    images2 = f['testing_Images'][()]
+    filenames2 = f['testing_image_filenames'][()]
+    
+images2 = images2.squeeze()
+filenames2 = filenames2.tolist()
+filenames2=[x.decode('utf-8') for x in filenames2]
+
+# Read .npy files
+
+seg1_slice_list_np = np.load(seg1_path)
+seg2_slice_list_np = np.load(seg2_path)
+
+expert1_slice_list_np = np.load(expert1_path)
+expert2_slice_list_np = np.load(expert2_path)
+
+expert1_slice_list_np = expert1_slice_list_np.squeeze()
+expert2_slice_list_np = expert2_slice_list_np.squeeze()
+
+
+# Specify Plot parameters
+
+expert1_color = '#FF0068'
+expert2_color = '#00C5FF'
+seg_color = '#00FF00'
+thickness = 2
+out_dpi = 400
+a = 0.5
+
+# Create plots
+out_path = root_out + "Whole/"
+for k in range(len(seg1_slice_list_np)):
+    plt.axis('off')
+    fig, ax = plt.subplots(1, 4, figsize=(20, 10))
+    
+    ax[0].imshow(images1[k], cmap='gray', interpolation='bilinear')
+    #ax[0].set_title('Image')
+    ax[0].grid(False)
+    
+    ax[1].imshow(images1[k], cmap='gray', interpolation='bilinear')
+    ax[1].contour(expert1_slice_list_np[k], colors=expert1_color, linewidths=thickness, alpha=a)
+    ax[1].contour(expert2_slice_list_np[k], colors=expert2_color, linewidths=thickness, alpha=a)
+    #ax[1].set_title('Image Annotated by 2 experts \n (Red = Expert 1 and Cyan = Expert 2)')
+    ax[1].grid(False)
+    
+    ax[2].imshow(images1[k], cmap='gray', interpolation='bilinear')
+    ax[2].contour(expert1_slice_list_np[k], colors=expert1_color, linewidths=thickness, alpha=a)
+    ax[2].contour(seg1_slice_list_np[k], colors=seg_color, linewidths=thickness, alpha=a)
+    #ax[2].set_title('U-Net Compared to Expert 1')
+    ax[2].grid(False)
+    
+    ax[3].imshow(images1[k], cmap='gray', interpolation='bilinear')
+    ax[3].contour(expert2_slice_list_np[k], colors=expert2_color, linewidths=thickness, alpha=a)
+    ax[3].contour(seg2_slice_list_np[k], colors=seg_color, linewidths=thickness, alpha=a)
+    #ax[3].set_title('U-Net Compared to Expert 2')
+    ax[3].grid(False)
+    
+    ax[0].set_axis_off()
+    ax[1].set_axis_off()
+    ax[2].set_axis_off()
+    ax[3].set_axis_off()
+    fig.savefig(out_path + filenames1[k] + '.png', bbox_inches='tight', dpi=out_dpi)
+    plt.close()
+    
+# Create Original Image plots
+out_path = root_out + "Original_Image/"
+for k in range(len(seg1_slice_list_np)):
+    plt.axis('off')
+    fig = plt.figure(frameon=False)
+    plt.imshow(images1[k], cmap='gray', interpolation='bilinear')
+    #plt.title('Image')
+    plt.grid(False)
+    plt.axis("off")
+    fig.savefig(out_path + filenames1[k] + '.png', bbox_inches='tight', dpi=out_dpi)
+    plt.close()
+    
+# Create image with expert annotation
+out_path = root_out + "Expert/"
+for k in range(len(seg1_slice_list_np)):
+    plt.axis('off')
+    fig = plt.figure(frameon=False)
+    plt.imshow(images1[k], cmap='gray', interpolation='bilinear')
+    plt.contour(expert1_slice_list_np[k], colors=expert1_color, linewidths=thickness, alpha=a)
+    plt.contour(expert2_slice_list_np[k], colors=expert2_color, linewidths=thickness, alpha=a)
+    #plt.title('Image with Expert Annotation')
+    plt.grid(False)
+    plt.axis("off")
+    fig.savefig(out_path + filenames1[k] + '.png', bbox_inches='tight', dpi=out_dpi)
+    plt.close()
+    
+# Create image with segmentation and expert annotation 1
+out_path = root_out + "Segmentation-Expert1/"
+for k in range(len(seg1_slice_list_np)):
+    plt.axis('off')
+    fig = plt.figure(frameon=False)
+    plt.imshow(images1[k], cmap='gray', interpolation='bilinear')
+    plt.contour(expert1_slice_list_np[k], colors=expert1_color, linewidths=thickness, alpha=a)
+    plt.contour(seg1_slice_list_np[k], colors=seg_color, linewidths=thickness, alpha=a)
+    #plt.title('Image with Expert Annotation')
+    plt.grid(False)
+    plt.axis("off")
+    fig.savefig(out_path + filenames1[k] + '.png', bbox_inches='tight', dpi=out_dpi)
+    plt.close()
+    
+# Create image with segmentation and expert annotation 2
+out_path = root_out + "Segmentation-Expert2/"
+for k in range(len(seg1_slice_list_np)):
+    plt.axis('off')
+    fig = plt.figure(frameon=False)
+    plt.imshow(images1[k], cmap='gray', interpolation='bilinear')
+    plt.contour(expert2_slice_list_np[k], colors=expert2_color, linewidths=thickness, alpha=a)
+    plt.contour(seg2_slice_list_np[k], colors=seg_color, linewidths=thickness, alpha=a)
+    #plt.title('Image with Expert Annotation')
+    plt.grid(False)
+    plt.axis("off")
+    fig.savefig(out_path + filenames1[k] + '.png', bbox_inches='tight', dpi=out_dpi)
+    plt.close()
